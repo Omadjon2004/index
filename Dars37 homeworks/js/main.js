30200bb6efa22918eefeeedde49a1489d48bb2c9
Omadjon2004 <122809872+Omadjon2004@users.noreply.github.com>
2023-03-20 17:09:44 +0500

    Add files via upload

---
+++ Dars37 homeworks/js/main.js
@@ -0,0 +1,43 @@
+           // TASK1
+
+// const firstFun = function(arr , element){
+//    let firstIndex = []
+//    let secondIndex = []
+//    let index =arr.indexOf(element)
+//     if(arr.includes(element)){
+// return index
+//     }else{
+//         return secondIndex
+//     }
+   
+// }
+
+// console.log(firstFun([1,2,3,5,6,4,8,9,10], 8));
+
+
+
+
+
+       //TASK 2
+
+  
+
+
+    // let people = [
+    //     { name: "Alice",
+    //       age: 25 },
+    //     { name: "Bob", 
+    //       age: 30 },
+    //     { name: "Charlie", 
+    //       age: 35 }
+
+          
+    //   ];
+
+    //   function isBigEnough(element) {
+    //     return element >= 30;
+    //   }
+    
+
+    //   console.log( people.every(isBigEnough));
+
